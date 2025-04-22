e78bb97c051a1cee2dba1a17412ddca0f4f4a3da
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-22 18:01:51 +0200

    Update and rename main.cpp to main.ino

---
+++ src/main.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <WiFi.h> // Include WiFi first
+
+WiFiClass WiFi; // Use WiFi object
+
+// Dummy handler to potentially use event types
+void WiFiEventHandler(arduino_event_id_t event) {
+Serial.printf("[WiFi-event] event: %d\n", event);
+}
+
+void setup() {
+Serial.begin(115200);
+Serial.println("Minimal ESP32-C6 Network Event Type Test");
+// WiFi.mode(WIFI_STA);
+}
+
+void loop() {
+delay(1000);
+}
